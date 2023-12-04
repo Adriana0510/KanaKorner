68cc8a6209018872429540ba8a7fd502585745ca
Adriana0510 <adriannacerna0510@gmail.com>
2023-12-04 00:39:34 -0600

    avance de pruebas
    Se realizaro el codigo para las pruebas de hiragana, katakana y kanji

--- ProyectoFinal/frontend/src/app/pages/prueba/prueba.component.ts
+++ ProyectoFinal/frontend/src/app/pages/prueba/prueba.component.ts
@@ -6,52 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./prueba.component.scss']
 })
 export class PruebaComponent {
-  preguntas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  respuestasHiragana: string[] = ["C","B","B","B","C","A","C","B","B","A"];
-  respuestasKatakana: string[] = ["C","B","C","B","A","C","C","B","A","A"];
-  respuestasKanji: string[] = ["A","C","B","A","B","C","B","B","A","C"];
+  userAnswers: any = {
+    question_1: {},
+    question_2: {},
+    question_3: {}
+  };
 
-  respuestasUsuarioHiragana: string[] = Array(10).fill('');
-  respuestasUsuarioKatakana: string[] = Array(10).fill('');
-  respuestasUsuarioKanji: string[] = Array(10).fill('');
+  correctAnswers: any = {
+    question_1: { 1: 'C', 2: 'B', 3: 'B', 4: 'B', 5: 'C', 6: 'A', 7: 'C', 8: 'B', 9: 'B', 10: 'A' },
+    question_2: { 1: 'C', 2: 'B', 3: 'C', 4: 'B', 5: 'A', 6: 'C', 7: 'C', 8: 'B', 9: 'A', 10: 'A' },
+    question_3: { 1: 'A', 2: 'C', 3: 'B', 4: 'A', 5: 'B', 6: 'C', 7: 'B', 8: 'B', 9: 'A', 10: 'C' }
+  };
 
-  seleccionarRespuestaHiragana(pregunta: number, respuesta: string) {
-    console.log(`Pregunta ${pregunta} (Hiragana): Respuesta seleccionada - ${respuesta}`);
-    // Almacena la respuesta del usuario
-    this.respuestasUsuarioHiragana[pregunta - 1] = respuesta;
+  results: any = {
+    question_1: '',
+    question_2: '',
+    question_3: ''
+  };
+
+  evaluateAnswers(question: string) {
+    const userAnswers = this.userAnswers[question];
+    const correctAnswers = this.correctAnswers[question];
+
+    const result = Object.keys(userAnswers).reduce((acc, key) => {
+      if (userAnswers[key] === correctAnswers[key]) {
+        acc.correct++;
+      } else {
+        acc.incorrect++;
+      }
+      return acc;
+    }, { correct: 0, incorrect: 0 });
+
+    this.results[question] = `Preguntas correctas: ${result.correct}`;
   }
 
-  evaluarHiragana() {
-    console.log('Evaluar respuestas de Hiragana');
-    // Lógica para evaluar respuestas de Hiragana
-    console.log('Respuestas de Hiragana del usuario:', this.respuestasUsuarioHiragana);
-    // Comparar con respuestas correctas (this.respuestasHiragana) y realizar la lógica de evaluación
-    // Por ejemplo, contar respuestas correctas, calcular puntajes, etc.
-  }
-
-  seleccionarRespuestaKatakana(pregunta: number, respuesta: string) {
-    console.log(`Pregunta ${pregunta} (Katakana): Respuesta seleccionada - ${respuesta}`);
-    // Almacena la respuesta del usuario
-    this.respuestasUsuarioKatakana[pregunta - 1] = respuesta;
-  }
-
-  evaluarKatakana() {
-    console.log('Evaluar respuestas de Katakana');
-    // Lógica para evaluar respuestas de Katakana
-    console.log('Respuestas de Katakana del usuario:', this.respuestasUsuarioKatakana);
-    // Comparar con respuestas correctas (this.respuestasKatakana) y realizar la lógica de evaluación
-  }
-
-  seleccionarRespuestaKanji(pregunta: number, respuesta: string) {
-    console.log(`Pregunta ${pregunta} (Kanji): Respuesta seleccionada - ${respuesta}`);
-    // Almacena la respuesta del usuario
-    this.respuestasUsuarioKanji[pregunta - 1] = respuesta;
-  }
-
-  evaluarKanji() {
-    console.log('Evaluar respuestas de Kanji');
-    // Lógica para evaluar respuestas de Kanji
-    console.log('Respuestas de Kanji del usuario:', this.respuestasUsuarioKanji);
-    // Comparar con respuestas correctas (this.respuestasKanji) y realizar la lógica de evaluación
+  resetQuiz(question: string) {
+    this.userAnswers[question] = {};
+    this.results[question] = ''; 
   }
 }
